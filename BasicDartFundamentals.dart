473188b7bb6ccdbd3e99ff59c241e5a4babe5c2b
Placksss <168385517+Placksss@users.noreply.github.com>
2025-08-04 08:30:54 +0000

    ACT 1 AUG 8

--- BasicDartFundamentals.dart
+++ BasicDartFundamentals.dart
@@ -1,1 +1,10 @@
-asd
+// void main() {
+//   String name = "Alice";
+//   print ("Welcome $name!");
+// } 
+
+  void main() {
+    dynamic name = "Alice";
+    name = 25;
+    print ("Welcome $name!");
+}
